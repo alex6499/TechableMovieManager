5dd705b3dc23c9d11ab291381c8d221f0e41c361
Alex Ginglen <aging2@uis.edu>
2016-10-02 15:08:06 -0500

    Added empty menus

---
+++ TechableMovieManager/TechableMovieManager/AdminControlsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechableMovieManager
+{
+    public partial class AdminControlsMenu : Form
+    {
+        public AdminControlsMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
