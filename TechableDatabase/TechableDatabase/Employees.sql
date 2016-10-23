58d0c8ce2ec9d4406e6ad25dce20bbe55488a42f
ithom2 <ithom2@CAP-ITHOM2>
2016-10-23 17:31:26 -0500

    Added Employee table

---
+++ TechableDatabase/TechableDatabase/Employees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstName] NCHAR(10) NOT NULL, 
+    [lastName] NCHAR(10) NOT NULL, 
+    [isAdmin] BIT NOT NULL, 
+    [userName] NCHAR(10) NOT NULL, 
+    [password] NCHAR(10) NOT NULL
+)
