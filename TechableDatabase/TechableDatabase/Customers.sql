123cf3506b52cda35c5264a903d874f64a9192f9
ithom2 <ithom2@CAP-ITHOM2>
2016-10-23 17:28:32 -0500

    Added Customer Table

---
+++ TechableDatabase/TechableDatabase/Customers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [lastName] NCHAR(10) NOT NULL, 
+    [firstName] NCHAR(10) NOT NULL, 
+    [email] NCHAR(10) NULL, 
+    [address] NCHAR(10) NULL, 
+    [phoneNumber] NCHAR(10) NULL
+)
